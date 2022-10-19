cd68c7a30a060c30e76966e57e2f1f7bceb57be6
Rojoin <nachoarrastua@gmail.com>
2022-10-19 20:51:08 -0300

    Add FMod

--- Assets/brokenMirror.cs
+++ Assets/brokenMirror.cs
@@ -1,20 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class brokenMirror : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer currentSpriteRenderer;
+    [SerializeField] private SpriteRenderer[] storedImages;
+    private int imageCounter = 0;
 
-    
     void Start()
     {
         currentSpriteRenderer = GetComponent<SpriteRenderer>();
+        currentSpriteRenderer = storedImages[imageCounter];
+        imageCounter = 0;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            imageCounter++;
+        }
+
+       
     }
+
+    void ChangeImage()
+    {
+        if (imageCounter == 10)
+        {
+            currentSpriteRenderer = storedImages[imageCounter / 10];
+        }
+    }
+    
 }
